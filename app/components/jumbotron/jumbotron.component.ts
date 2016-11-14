f5fa03784e782aace830e11b37f83878211277c4
Winder Ojeda <winder.alejandro@gmail.com>
2016-11-14 16:33:13 -0400

    develop start

---
+++ app/components/jumbotron/jumbotron.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+@Component({
+  moduleId: module.id,
+  selector: 'jumbotron',
+  templateUrl: 'jumbotron.component.html'
+})
+export class JumbotronComponent { 
+	private jbtHeading:string;
+	private jbtText:string;
+	private jbtBtnText:string;
+	private jbtBtnUrl:string;
+
+	constructor(){
+		this.jbtHeading = "Hello World";
+		this.jbtText = "Simple Text";
+		this.jbtBtnText = "Read More";
+		this.jbtBtnUrl ="/about";
+	}
+}
